cbd5b0d48164c9f3a278d22acbdd1e5b9be2fcd1
Vladimir Adamić <441333+Vunovati@users.noreply.github.com>
2023-07-21 12:52:52 +0200

    Test ci (#2)
    * Modify tests to use otlp collector directly
    
    - inspect otlp collector to assert correctl behaviour
    
    * Run all test cases in single suite
    
    * Run collector with docker run instead of docker-compose
    
    * Add service name resource
    
    * Extract otlp-logger so it can be unit tested
    
    * Update README with instructions for running locally
    
    * Run on CI on windows and ubuntu with all maintained node.js versions
    
    * drop support for node 14
    
    * lower unit test branch coverage to 60%

--- test/pino-opentelemetry-transport.test.js
+++ test/pino-opentelemetry-transport.test.js
@@ -1,40 +1,25 @@
 'use strict'
 
-const { mkdir, rm, readFile } = require('fs/promises')
+const { readFile } = require('fs/promises')
+const { truncateSync } = require('fs')
 const { join } = require('path')
-const { test, beforeEach } = require('tap')
+const { test, afterEach } = require('tap')
 const { promisify } = require('util')
-const { tmpdir } = require('os')
 const requireInject = require('require-inject')
-
-async function cleanAndCreateFolder (path) {
-  await rm(path, { force: true, recursive: true })
-  await mkdir(path, { force: true, recursive: true })
-}
 
 const sleep = promisify(setTimeout)
 
-const logFolder = join(tmpdir(), 'test-logs')
+const logFile = join('/', 'tmp', 'test-logs', 'otlp-logs.log')
+afterEach(() => {
+  truncateSync(logFile)
+})
 
-beforeEach(() => cleanAndCreateFolder(logFolder))
-
-const logFile = join(logFolder, 'pino.log')
 const MOCK_HOSTNAME = 'hostname'
 
 test('translate Pino log format to Open Telemetry data format for each log level', async ({
   same,
-  afterEach
+  hasStrict
 }) => {
-  const ms = 1531069919686
-  const now = Date.now
-  Date.now = () => ms
-
-  afterEach(() => {
-    Date.now = now
-
-    return rm(logFolder, { force: true, recursive: true })
-  })
-
   const pino = requireInject.withEmptyCache('pino', {
     os: {
       hostname: () => MOCK_HOSTNAME
@@ -46,233 +31,236 @@ test('translate Pino log format to Open Telemetry data format for each log level
     target: '..',
     options: {
       loggerName: 'test-logger-name',
-      serviceVersion: 'test-service-version'
+      serviceVersion: 'test-service-version',
+      serviceName: 'test-service',
+      useBatchProcessor: false // make sure all logs are flushed immediately
     }
   })
 
-  const logger = pino(transport)
+  const logger = pino(transport, {})
   logger.level = 'trace'
 
   logger.trace('test trace')
+  await sleep(500)
   logger.debug('test debug')
+  await sleep(500)
   logger.info('test info')
+  await sleep(500)
   logger.warn('test warn')
+  await sleep(500)
   logger.error('test error')
+  await sleep(500)
   logger.fatal('test fatal')
+  await sleep(500)
 
-  await sleep(100)
-  const content = await readFile(logFile, 'utf8')
-
-  const Timestamp = ms + '0'.repeat(6)
-
-  const Resource = {
-    'host.hostname': MOCK_HOSTNAME,
-    'process.pid': process.pid
+  const resource = {
+    attributes: [
+      {
+        key: 'service.name',
+        value: {
+          stringValue: 'test-service'
+        }
+      },
+      {
+        key: 'telemetry.sdk.language',
+        value: { stringValue: 'nodejs' }
+      },
+      {
+        key: 'telemetry.sdk.name',
+        value: { stringValue: 'opentelemetry' }
+      },
+      { key: 'telemetry.sdk.version', value: { stringValue: '1.15.0' } }
+    ]
   }
 
-  const Attributes = {}
+  const scope = {
+    name: 'test-logger-name',
+    version: 'test-service-version'
+  }
+
+  const extra = {
+    foo: 'bar',
+    baz: 'qux'
+  }
+
+  logger.trace(extra, 'test trace')
+
+  await sleep(500)
+
+  const content = await readFile(logFile, 'utf8')
+
+  const lines = content
+    .split('\n')
+    .filter(Boolean)
+    .filter(line => line.startsWith('{'))
 
   const expectedLines = [
     {
-      Body: 'test trace',
-      Timestamp,
-      SeverityNumber: 1,
-      SeverityText: 'TRACE',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 1,
+                  severityText: 'TRACE',
+                  body: { stringValue: 'test trace' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      Body: 'test debug',
-      Timestamp,
-      SeverityNumber: 5,
-      SeverityText: 'DEBUG',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 5,
+                  severityText: 'DEBUG',
+                  body: { stringValue: 'test debug' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      Body: 'test info',
-      Timestamp,
-      SeverityNumber: 9,
-      SeverityText: 'INFO',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 9,
+                  severityText: 'INFO',
+                  body: { stringValue: 'test info' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      Body: 'test warn',
-      Timestamp,
-      SeverityNumber: 13,
-      SeverityText: 'WARN',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 13,
+                  severityText: 'WARN',
+                  body: { stringValue: 'test warn' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      Body: 'test error',
-      Timestamp,
-      SeverityNumber: 17,
-      SeverityText: 'ERROR',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 17,
+                  severityText: 'ERROR',
+                  body: { stringValue: 'test error' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
-      Body: 'test fatal',
-      Timestamp,
-      SeverityNumber: 21,
-      SeverityText: 'FATAL',
-      Resource,
-      Attributes
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 21,
+                  severityText: 'FATAL',
+                  body: { stringValue: 'test fatal' },
+                  traceId: '',
+                  spanId: ''
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      resourceLogs: [
+        {
+          resource,
+          scopeLogs: [
+            {
+              scope,
+              logRecords: [
+                {
+                  severityNumber: 1,
+                  severityText: 'TRACE',
+                  body: { stringValue: 'test trace' },
+                  traceId: '',
+                  spanId: '',
+                  attributes: [
+                    { key: 'foo', value: { stringValue: 'bar' } },
+                    { key: 'baz', value: { stringValue: 'qux' } }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   ]
 
-  const logEntries = content.split('\n').filter(Boolean).map(JSON.parse)
+  same(lines.length, expectedLines.length, 'correct number of lines')
 
-  for (const [lineNumber, logLine] of logEntries.entries()) {
-    same(
-      logLine,
+  for (const [lineNumber, logLine] of lines.entries()) {
+    hasStrict(
+      JSON.parse(logLine),
       expectedLines[lineNumber],
       `line ${lineNumber} severity is mapped correctly`
     )
   }
+
+  hasStrict(
+    JSON.parse(lines[lines.length - 1]),
+    expectedLines[expectedLines.length - 1],
+    'extra bindings are stored'
+  )
 })
-
-// test('translate Pino log format to Open Telemetry data and store all extra bindings as attributes', async ({ afterEach, equal, same }) => {
-//   const ms = 1531069919686
-//   const now = Date.now
-//   Date.now = () => ms
-//
-//   afterEach(() => {
-//     Date.now = now
-//
-//     return rm(logFolder, { force: true, recursive: true })
-//   })
-//
-//   const pino = requireInject.withEmptyCache('pino', {
-//     os: {
-//       hostname: () => MOCK_HOSTNAME
-//     }
-//   })
-//
-//   const transport = pino.transport({
-//     level: 'trace',
-//     target: '..',
-//     options: { destination: logFile }
-//   })
-//
-//   const logger = pino(transport)
-//   logger.level = 'trace'
-//
-//   const extra = {
-//     foo: 'bar',
-//     baz: 'qux'
-//   }
-//
-//   logger.trace(extra, 'test trace')
-//
-//   await sleep(100)
-//   const content = await readFile(logFile, 'utf8')
-//
-//   const Timestamp = ms + '0'.repeat(6)
-//
-//   const Resource = {
-//     'host.hostname': MOCK_HOSTNAME,
-//     'process.pid': process.pid
-//   }
-//
-//   const Attributes = extra
-//
-//   const expectedLine = { Body: 'test trace', Timestamp, SeverityNumber: 1, SeverityText: 'TRACE', Resource, Attributes }
-//
-//   const logEntries = content.split('\n').filter(Boolean).map(JSON.parse)
-//
-//   equal(logEntries.length, 1, 'only one log entry is written')
-//   same(logEntries[0], expectedLine, 'log entry contains all extra bindings as attributes')
-// })
-//
-// test('translate Pino log format to Open Telemetry data with custom messageKey', async ({ afterEach, equal, same }) => {
-//   const ms = 1531069919686
-//   const now = Date.now
-//   Date.now = () => ms
-//
-//   afterEach(() => {
-//     Date.now = now
-//
-//     return rm(logFolder, { force: true, recursive: true })
-//   })
-//
-//   const pino = requireInject.withEmptyCache('pino', {
-//     os: {
-//       hostname: () => MOCK_HOSTNAME
-//     }
-//   })
-//
-//   const MESSAGE_KEY = 'customMessageKey'
-//
-//   const transport = pino.transport({
-//     level: 'trace',
-//     target: '..',
-//     options: { destination: logFile, messageKey: MESSAGE_KEY }
-//   })
-//
-//   const logger = pino(transport)
-//   logger.level = 'trace'
-//
-//   const extra = {
-//     foo: 'bar',
-//     baz: 'qux',
-//     [MESSAGE_KEY]: 'test message for message key'
-//   }
-//
-//   logger.trace(extra, 'test trace')
-//
-//   await sleep(100)
-//   const content = await readFile(logFile, 'utf8')
-//
-//   const Timestamp = ms + '0'.repeat(6)
-//
-//   const Resource = {
-//     'host.hostname': MOCK_HOSTNAME,
-//     'process.pid': process.pid
-//   }
-//
-//   const Attributes = {
-//     foo: 'bar',
-//     baz: 'qux',
-//     msg: 'test trace'
-//   }
-//
-//   const expectedLine = { Body: 'test message for message key', Timestamp, SeverityNumber: 1, SeverityText: 'TRACE', Resource, Attributes }
-//
-//   const logEntries = content.split('\n').filter(Boolean).map(JSON.parse)
-//
-//   equal(logEntries.length, 1, 'only one log entry is written')
-//   same(logEntries[0], expectedLine, 'the log line interprets the custom messageKey value as Body')
-// })
-
-// test('handle backpressure', async ({ afterEach, equal }) => {
-//   afterEach(() => {
-//     return rm(logFolder, { force: true, recursive: true })
-//   })
-//
-//   const pino = require('pino')
-//
-//   const transport = pino.transport({
-//     level: 'trace',
-//     target: '..',
-//     options: { destination: logFile }
-//   })
-//
-//   const logger = pino(transport)
-//   logger.level = 'trace'
-//
-//   const N_LOG_LINES = 10_000
-//   for (let i = 0; i < N_LOG_LINES; i++) {
-//     logger.trace('test trace')
-//   }
-//
-//   await sleep(1000)
-//
-//   const content = await readFile(logFile, 'utf8')
-//
-//   const logEntries = content.split('\n').filter(Boolean).map(JSON.parse)
-//
-//   equal(logEntries.length, N_LOG_LINES, 'all log lines are written')
-// })
