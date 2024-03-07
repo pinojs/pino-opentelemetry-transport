1ba176684cb5482717abc8b09a9fc62c5421228d
Samuel Vicente <samuellvicente@users.noreply.github.com>
2024-03-07 09:17:40 +0000

    docs: fix wrong path in minimalistic example (#145)

--- examples/minimalistic/minimalistic.js
+++ examples/minimalistic/minimalistic.js
@@ -4,7 +4,7 @@ const path = require('path')
 const pino = require('pino')
 
 const transport = pino.transport({
-  target: path.join(__dirname, '..', '..', 'pino-opentelemetry-transport')
+  target: path.join(__dirname, '..', '..', 'lib', 'pino-opentelemetry-transport')
 })
 
 const logger = pino(transport)
